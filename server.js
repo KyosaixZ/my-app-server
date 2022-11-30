18d83f7021a795ebeb6307668b8a1403be8189e6
KyosaixZ <kyosaix@hotmail.com>
2022-11-30 18:27:25 +0700

    add free and employ in server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const User = require('./models/users');
+const Freelance = require('./models/freelances');
+const Employer = require('./models/employers');
 
 app.use(express.json())
 
@@ -17,6 +19,16 @@ app.get('/users', async (req, res) => {
   console.log(users);
   res.json(users);
 });
+app.get('/freelances', async (req, res) => {
+  const freelances = await Freelance.find({});
+  console.log(freelances);
+  res.json(freelances);
+});
+app.get('/employers', async (req, res) => {
+  const employers = await Employer.find({});
+  console.log(employers);
+  res.json(employers);
+});
 
 //get one
 app.get('/users/:id', async (req, res) => {
@@ -24,12 +36,34 @@ app.get('/users/:id', async (req, res) => {
   const user = await User.findById(id);
   res.json(user);
 });
+app.get('/freelances/:id', async (req, res) => {
+  const { id } = req.params;
+  const freelance = await Freelance.findById(id);
+  res.json(freelance);
+});
+app.get('/employers/:id', async (req, res) => {
+  const { id } = req.params;
+  const employer = await Employer.findById(id);
+  res.json(employer);
+});
 
 //Post
 app.post('/users', async (req, res) => {
   const payload = req.body;
   const user = new User(payload);
   await user.save();
+  res.status(201).end();
+});
+app.post('/freelances', async (req, res) => {
+  const payload = req.body;
+  const freelance = new Freelance(payload);
+  await freelance.save();
+  res.status(201).end();
+});
+app.post('/employers', async (req, res) => {
+  const payload = req.body;
+  const employer = new Employer(payload);
+  await employer.save();
   res.status(201).end();
 });
 
@@ -41,6 +75,20 @@ app.put('/users/:id', async (req, res) => {
   const user = await User.findByIdAndUpdate(id, { $set: payload });
   res.json(user);
 });
+app.put('/freelances/:id', async (req, res) => {
+  const payload = req.body;
+  const { id } = req.params;
+
+  const freelance = await Freelance.findByIdAndUpdate(id, { $set: payload });
+  res.json(freelance);
+});
+app.put('/employers/:id', async (req, res) => {
+  const payload = req.body;
+  const { id } = req.params;
+
+  const employer = await Employer.findByIdAndUpdate(id, { $set: payload });
+  res.json(employer);
+});
 
 //Delete
 app.delete('/users/:id', async (req, res) => {
@@ -49,6 +97,19 @@ app.delete('/users/:id', async (req, res) => {
   await User.findByIdAndDelete(id);
   res.status(204).end();
 });
+app.delete('/freelances/:id', async (req, res) => {
+  const { id } = req.params;
+
+  await Freelance.findByIdAndDelete(id);
+  res.status(204).end();
+});
+app.delete('/employers/:id', async (req, res) => {
+  const { id } = req.params;
+
+  await Employer.findByIdAndDelete(id);
+  res.status(204).end();
+});
+
 
 app.listen(9000, () => {
   console.log('Application is running on port 9000');
